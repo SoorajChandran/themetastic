5768bad083661dda8f12bfc14a42efd9f1daf67c
Sooraj Chandran <soorajchn@gmail.com>
2020-02-13 13:59:32 +0100

    Updates

---
+++ precache-manifest.c6e4b806ef9b71a974053f7ba744243d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7157215f7f8a557c999686397f3494f",
+    "url": "/themetastic/index.html"
+  },
+  {
+    "revision": "0cd0ba2b7953e487e22a",
+    "url": "/themetastic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bebf29c20b0c9d1e7362",
+    "url": "/themetastic/static/js/2.992f277b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/themetastic/static/js/2.992f277b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cd0ba2b7953e487e22a",
+    "url": "/themetastic/static/js/main.ab79d6a3.chunk.js"
+  },
+  {
+    "revision": "efa2ba5a36ab38c59f5f",
+    "url": "/themetastic/static/js/runtime-main.f15d0d14.js"
+  }
+]);
